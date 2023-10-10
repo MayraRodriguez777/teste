65562f50e43d9aa901ba772aa7033c2bc24cac4d
MayraRodriguez777 <mayyra.machado@gmail.com>
2023-10-10 18:00:41 +0100

    cadastro dentista

--- Models/AppDBContext.cs
+++ Models/AppDBContext.cs
@@ -4,10 +4,13 @@ namespace Eassydentalmvc.Models
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext (DbContextOptions<AppDbContext> options) : base (options){ } 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
         public DbSet<Cliente> Clientes { get; set; }
-    
+        public DbSet<Dentista> Dentistas { get; set; }
+
+        // Outros DbSets para outras entidades, se houver
     }
-
-
 }
